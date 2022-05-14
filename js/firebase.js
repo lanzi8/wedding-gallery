d26c9d552108201ae598d752ace44cc0d90ed7e4
Lanzerstorfer, Franz (RC-AT PD PA EM) <franz.lanzerstorfer.ext@siemens.com>
2022-05-14 22:04:23 +0200

    Initial commit

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0vcONC2MF7wMKpnS5XMvYjuM9L5tAQxM",
+  authDomain: "lanz-wedding-gallery.firebaseapp.com",
+  projectId: "lanz-wedding-gallery",
+  storageBucket: "lanz-wedding-gallery.appspot.com",
+  messagingSenderId: "148570505253",
+  appId: "1:148570505253:web:8841848ed74dfe4b517fcc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
